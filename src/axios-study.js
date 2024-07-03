b17a02e76f9a0dc9abefe597c61f0033e814f8dc
Shubhamh2612 <87204751+Shubhamh2612@users.noreply.github.com>
2024-07-03 10:35:05 +0530

    Update axios-study.js

--- src/axios-study.js
+++ src/axios-study.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/';
+const baseURL = 'https://auction-management-system-server.onrender.com/';
 
 const instance = axios.create({
   baseURL: baseURL
